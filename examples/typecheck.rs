2b2825201e9be52e453d0f5ac45b5dc08508bbbf
Henry Blanchette <henry@monkeyface.com>
2025-10-15 11:29:04 -0400

    write check_sort and check_proof in a way that constructs a new expr with the appropriate annotations

--- examples/typecheck.rs
+++ examples/typecheck.rs
@@ -1,4 +1,4 @@
-#![allow(dead_code)]
+#![allow(dead_code, clippy::println_empty_string, non_upper_case_globals)]
 
 use lazy_static::lazy_static;
 use map_macro::hash_map;
@@ -13,7 +13,7 @@ struct Expr {
 
 impl Display for Expr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "ex!(\"{}\"", self.label)?;
+        write!(f, "ex!({}", self.label)?;
         if !self.kids.is_empty() {
             write!(f, ", [")?;
             for (i, kid) in self.kids.iter().enumerate() {
@@ -25,7 +25,7 @@ impl Display for Expr {
             write!(f, "]")?;
         }
         if let Some(ann) = &self.annotation {
-            write!(f, ", \"{ann}\"")?;
+            write!(f, ", {ann}")?;
         }
         write!(f, ")")
     }
@@ -34,6 +34,10 @@ impl Display for Expr {
 impl Expr {
     pub fn pat(&self) -> (&str, &[Self]) {
         (self.label.as_str(), self.kids.as_slice())
+    }
+
+    pub fn pat_owned<const N: usize>(self) -> (String, [Self; N]) {
+        (self.label, self.kids.try_into().expect("impossible"))
     }
 }
 
@@ -45,14 +49,14 @@ macro_rules! ex {
             kids: vec![]
         }
     };
-    ( $label:expr, [ $( $kid: expr ),* ] ) => {
+    ( $label: expr, [ $( $kid: expr ),* ] ) => {
         Expr {
             label: $label.to_owned(),
             annotation: None,
             kids: vec![ $( $kid ),* ]
         }
     };
-    ( $label:expr, [ $( $kid: expr ),* ], $ann: literal ) => {
+    ( $label: expr, [ $( $kid: expr ),* ], $ann: expr ) => {
         Expr {
             label: $label.to_owned(),
             annotation: $ann.to_owned(),
@@ -105,11 +109,17 @@ macro_rules! string_hash_map {
     };
 }
 
+const and: &str = "and";
+const top: &str = "top";
+const bot: &str = "bot";
+const intro_and: &str = "intro_and";
+const intro_top: &str = "intro_top";
+
 lazy_static! {
     static ref GRAMMAR: HashMap<String, Rule> = string_hash_map! {
         // Prop
-        "true" => rule!(Prop),
-        "false" => rule!(Prop),
+        "top" => rule!(Prop),
+        "bot" => rule!(Prop),
         "<" => rule!(Prop, [Num, Num]),
         ">" => rule!(Prop, [Num, Num]),
         ">=" => rule!(Prop, [Num, Num]),
@@ -132,20 +142,23 @@ lazy_static! {
         "ceil" => rule!(Num, [Num]),
         "floor" => rule!(Num, [Num]),
         // proof forms
-        "and_intro" => rule!(Proof, [Proof, Proof]),
-        "true_intro" => rule!(Proof),
+        "intro_and" => rule!(Proof, [Proof, Proof]),
+        "intro_top" => rule!(Proof),
+        "elim_bot" => rule!(Proof, [Proof]),
     };
 }
 
 // proof forms:
-//   - and_intro
-//   - and_elim
-//   - or_intro
-//   - or_elim
-//   - intro
-//   - exists
-//   - apply
-//   - rewrite
+//   - [x] intro_top
+//   - [x] elim_bot
+//   - [x] intro_and
+//   - [ ] elim_and
+//   - [ ] intro_or
+//   - [ ] elim_or
+//   - [ ] intro_var
+//   - [ ] exists
+//   - [ ] apply
+//   - [ ] rewrite
 
 fn from_vec_to_array<T, const N: usize>(xs: Vec<T>) -> Result<[T; N], Vec<T>> {
     xs.try_into()
@@ -153,7 +166,13 @@ fn from_vec_to_array<T, const N: usize>(xs: Vec<T>) -> Result<[T; N], Vec<T>> {
 
 fn check_sort(ctx: HashMap<String, Type>, expected_sort: &Sort, expr: Expr) -> Expr {
     let Rule { sort, kids } = match GRAMMAR.get(&expr.label) {
-        None => return expr,
+        None => {
+            return Expr {
+                label: expr.label,
+                annotation: Some("unknown expr label".to_owned()),
+                kids: expr.kids,
+            };
+        }
         Some(rule) => rule,
     };
     if sort != expected_sort {
@@ -179,7 +198,7 @@ fn check_sort(ctx: HashMap<String, Type>, expected_sort: &Sort, expr: Expr) -> E
 
     match expr.label.as_str() {
         "var" => {
-            let [x] = from_vec_to_array::<_, 1>(expr.kids).expect("impossible");
+            let [x] = from_vec_to_array(expr.kids).expect("impossible");
             if !ctx.contains_key(&x.label) {
                 return Expr {
                     label: expr.label,
@@ -194,7 +213,7 @@ fn check_sort(ctx: HashMap<String, Type>, expected_sort: &Sort, expr: Expr) -> E
             }
         }
         "forall" | "exists" => {
-            let [x, p] = from_vec_to_array::<_, 2>(expr.kids).expect("impossible");
+            let [x, p] = from_vec_to_array(expr.kids).expect("impossible");
             let ctx = {
                 let mut ctx = ctx;
                 ctx.insert(x.label.clone(), Type::Num);
@@ -233,57 +252,116 @@ enum Type {
 /// Checks that a proof is valid with respect to an expected [`Prop`].
 /// - Assumes that all the [`Type`]s in the context have been sort-checked.
 /// - Assumes that [`expected_prop`] has been sort-checked.
-fn check_proof(ctx: HashMap<String, Type>, expected_prop: &Expr, proof: &Expr) -> bool {
+fn check_proof(ctx: HashMap<String, Type>, expected_prop: &Expr, proof: Expr) -> Expr {
     // sort-check
     let Rule { sort, kids } = match GRAMMAR.get(&proof.label) {
-        None => return false,
+        None => {
+            return Expr {
+                label: proof.label,
+                annotation: Some("unknown expr label".to_owned()),
+                kids: proof.kids,
+            };
+        }
         Some(rule) => rule,
     };
     if sort != &Proof {
-        return false;
+        return Expr {
+            label: proof.label,
+            annotation: Some(format!(
+                "expected expr to have sort Proof, but actually has sort {sort}"
+            )),
+            kids: proof.kids,
+        };
     }
     if proof.kids.len() != kids.len() {
-        return false;
+        return Expr {
+            label: proof.label,
+            annotation: Some(format!(
+                "expected expr to have {} kids, but actually has {} kids",
+                kids.len(),
+                proof.kids.len()
+            )),
+            kids: proof.kids,
+        };
     }
 
     // proof-check
-    match (proof.pat(), expected_prop.pat()) {
-        (("and_intro", [a, b]), ("and", [p, q])) => {
-            check_proof(ctx.clone(), p, a) && check_proof(ctx, q, b)
+    match (proof.label.as_str(), expected_prop.pat()) {
+        ("intro_and", ("and", [p, q])) => {
+            let [a, b] = from_vec_to_array(proof.kids).expect("impossible");
+            let a = check_proof(ctx.clone(), p, a);
+            let b = check_proof(ctx.clone(), q, b);
+            Expr {
+                label: proof.label,
+                annotation: None,
+                kids: vec![a, b],
+            }
         }
-        (("true_intro", []), ("true", [])) => true,
-        _ => false,
+        ("intro_top", ("top", [])) => Expr {
+            label: proof.label,
+            annotation: None,
+            kids: vec![],
+        },
+        ("elim_bot", _) => {
+            let [a] = from_vec_to_array(proof.kids).expect("impossible");
+            let a = check_proof(ctx, &ex!["bot"], a);
+            Expr {
+                label: proof.label,
+                annotation: None,
+                kids: vec![a],
+            }
+        }
+        _ => Expr {
+            label: proof.label,
+            annotation: Some(format!(
+                "this proof is not a valid proof of the prop {expected_prop}"
+            )),
+            kids: proof.kids,
+        },
     }
 }
 
 fn test(ctx: &HashMap<String, Type>, expected_prop: Expr, proof: Expr) {
     let expected_prop = check_sort(ctx.clone(), &Prop, expected_prop);
     let proof = check_sort(ctx.clone(), &Proof, proof);
-    let result = check_proof(ctx.clone(), &expected_prop, &proof);
+    let result = check_proof(ctx.clone(), &expected_prop, proof.clone());
+    println!("");
     println!("{ctx:?} |-");
-    println!("     {proof} : {expected_prop}");
+    println!("     {proof}");
+    println!("        : {expected_prop}");
     println!(" ==> {result}");
 }
 
 pub fn main() {
     test(
         &hash_map! {},
-        ex!("and", [ex!("true"), ex!("true")]),
-        ex!("and_intro", [ex!("true_intro"), ex!("true_intro")]),
+        ex!(and, [ex!(top), ex!(top)]),
+        ex!(intro_and, [ex!(intro_top), ex!(intro_top)]),
     );
     test(
         &hash_map! {},
-        ex!("and", [ex!("true"), ex!("false")]),
-        ex!("and_intro", [ex!("true_intro"), ex!("true_intro")]),
+        ex!(and, [ex!(top), ex!(bot)]),
+        ex!(intro_and, [ex!(intro_top), ex!(intro_top)]),
     );
     test(
         &hash_map! {},
-        ex!("and", [ex!("false"), ex!("true")]),
-        ex!("and_intro", [ex!("true_intro"), ex!("true_intro")]),
+        ex!(and, [ex!(bot), ex!(top)]),
+        ex!(intro_and, [ex!(intro_top), ex!(intro_top)]),
     );
     test(
         &hash_map! {},
-        ex!("and", [ex!("false"), ex!("false")]),
-        ex!("and_intro", [ex!("true_intro"), ex!("true_intro")]),
+        ex!(and, [ex!(bot), ex!(bot)]),
+        ex!(intro_and, [ex!(intro_top), ex!(intro_top)]),
+    );
+    test(
+        &hash_map! {},
+        ex!("and", [ex!(top), ex!(top)]),
+        ex!(
+            intro_and,
+            [
+                ex!(intro_and, [ex!(intro_top), ex!(intro_top)]),
+                ex!(intro_top)
+            ]
+        ),
     );
 }
