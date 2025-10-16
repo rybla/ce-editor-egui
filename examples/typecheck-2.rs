637093d93abb105f1019cbfc214561c303dfdac4
Henry Blanchette <henry@rybl.net>
2025-10-16 15:41:06 -0400

    checkpoint in check_type
    Co-authored-by: Jacob Prinz <jacobeliasprinz@gmail.com>

--- examples/typecheck-2.rs
+++ examples/typecheck-2.rs
@@ -5,7 +5,7 @@ use map_macro::hash_map;
 use std::fmt::Debug;
 use std::{collections::HashMap, fmt::Display};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Expr<Ann> {
     label: String,
     annotation: Ann,
@@ -14,6 +14,8 @@ struct Expr<Ann> {
 
 /// annotated expr
 type AnnExpr<'a> = Expr<&'a mut Vec<String>>;
+/// unannotated expr
+type PlainExpr = Expr<()>;
 
 impl<Ann> Display for Expr<Ann> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -43,41 +45,60 @@ impl<Ann: Debug> Expr<Ann> {
     pub fn pat_owned<const N: usize>(self) -> (String, [Self; N]) {
         (self.label, self.kids.try_into().expect("impossible"))
     }
-}
 
-// impl<'a> AnnExpr<'a> {
-//     pub fn annotate<'a>(self, msg: String) -> Self {
-//         let mut expr = self;
-//         expr.annotation = &mut vec![msg];
-//         expr
-//     }
-// }
+    pub fn forget_ann(self) -> PlainExpr {
+        let Self { label, kids, .. } = self;
+        Expr {
+            label: label.clone(),
+            annotation: (),
+            kids: kids.into_iter().map(|x| x.forget_ann()).collect(),
+        }
+    }
+}
 
 macro_rules! ex {
     ( $label: expr ) => {
         Expr {
             label: $label.to_owned(),
-            annotation: None,
+            annotation: (),
             kids: vec![]
         }
     };
     ( $label: expr, [ $( $kid: expr ),* ] ) => {
         Expr {
             label: $label.to_owned(),
-            annotation: None,
+            annotation: (),
             kids: vec![ $( $kid ),* ]
         }
     };
     ( $label: expr, [ $( $kid: expr ),* ], $ann: expr ) => {
         Expr {
             label: $label.to_owned(),
-            annotation: $ann.to_owned(),
+            annotation: $ann,
             kids: vec![ $( $kid ),* ]
         }
     };
 }
 
+macro_rules! ex_ann {
+    ( $label: expr ) => {
+        Expr {
+            label: $label.to_owned(),
+            annotation: &mut vec![],
+            kids: vec![],
+        }
+    };
+    ( $label: expr, [ $( $kid: expr ),* ] ) => {
+        Expr {
+            label: $label.to_owned(),
+            annotation: &mut vec![],
+            kids: vec![],
+        }
+    };
+}
+
 #[derive(Debug, Clone, Eq, PartialEq, PartialOrd, Ord, Hash)]
+// this is like Type but with less information
 enum Sort {
     Proof,
     Prop,
@@ -85,11 +106,43 @@ enum Sort {
     Var,
 }
 
+impl Sort {
+    pub fn to_type(&self) -> Type<'_> {
+        match self {
+            Proof => {
+                panic!("this shouldn't be able to happen, can't recover details of sort to type")
+            }
+            Prop => Type::Prop,
+            Num => Type::Num,
+            Var => Type::Var,
+        }
+    }
+}
+
 use Sort::{Num, Proof, Prop, Var};
 
 impl Display for Sort {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{self:?}")
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Type<'a> {
+    Prop,
+    Num,
+    Var,
+    Proof(&'a PlainExpr),
+}
+
+impl<'a> Type<'a> {
+    pub fn get_sort(&self) -> Sort {
+        match self {
+            Type::Prop => Prop,
+            Type::Num => Num,
+            Type::Var => Var,
+            Type::Proof(_) => Proof,
+        }
     }
 }
 
@@ -177,49 +230,57 @@ fn from_expr_kids_to_array<T: Debug, const N: usize>(xs: Vec<T>) -> [T; N] {
         .expect("impossible since we already checked the arity of expr")
 }
 
-fn check_sort<'a>(ctx: HashMap<String, Type>, expected_sort: &Sort, expr: AnnExpr<'a>) -> bool {
+fn check_type<'a>(
+    ctx: HashMap<String, Type<'a>>,
+    expected_type: &Type<'a>,
+    expr: &AnnExpr<'a>,
+) -> bool {
     let mut success = true;
-    check_sort_helper(&mut success, ctx, expected_sort, expr);
+    check_type_helper(&mut success, ctx, &expected_type, expr);
     success
 }
 
-fn check_sort_helper<'a>(
+fn check_type_helper<'e, 'a>(
     success: &mut bool,
-    ctx: HashMap<String, Type>,
-    expected_sort: &Sort,
-    expr: AnnExpr<'a>,
+    ctx: HashMap<String, Type<'a>>,
+    expected_type: &Type<'a>,
+    expr: &AnnExpr<'a>,
 ) {
+    let expected_sort = expected_type.get_sort();
     // clean up the old annotation
     expr.annotation.drain(..);
 
     // call this function to add error annotation, also sets the success flag
     let mut add_error = |msg: String| {
         expr.annotation.push(msg);
-        *success = false
+        *success = false;
     };
 
     // get the corresponding rule
-    let Rule { sort, kids } = match GRAMMAR.get(&expr.label) {
+    let Rule {
+        sort: rule_sort,
+        kids: rule_kids,
+    } = match GRAMMAR.get(&expr.label) {
         None => {
             add_error("unknown expr label".to_owned());
-            return ();
+            return;
         }
         Some(rule) => rule,
     };
 
     // check sort
-    if sort != expected_sort {
+    if rule_sort != &expected_sort {
         add_error(format!(
-            "this expr was expected to have sort {expected_sort} but actually has sort {sort}"
+            "this expr was expected to have sort {expected_sort} but actually has sort {rule_sort}"
         ));
     }
 
     // check arity
-    if expr.kids.len() != kids.len() {
+    if expr.kids.len() != rule_kids.len() {
         let expr_kids_len = expr.kids.len();
         add_error(format!(
             "this expr was expected to have {} kids but actually has {} kids",
-            kids.len(),
+            rule_kids.len(),
             expr_kids_len
         ));
     }
@@ -227,142 +288,85 @@ fn check_sort_helper<'a>(
     // TODO: write a function (instead of from_expr_kids_to_array) that takes the number of expected kids, the actual kids, and returns an array of exprs of the expected number of kids len, buffered by some extra dummy kids, and all extra kids are just given an annotation error that indicates they're out of place
 
     // check kid sorts
-    match expr.label.as_str() {
-        "var" => {
-            let [x] = from_expr_kids_to_array(expr.kids);
+    match ((expr.label.as_str(), expr.kids.as_slice()), expected_type) {
+        (("var", [x]), _) => {
             if !ctx.contains_key(&x.label) {
                 add_error("this var expr is out-of-scope".to_owned());
             }
         }
-        "forall" | "exists" => {
-            let [x, p] = from_expr_kids_to_array(expr.kids);
+        (("forall" | "exists", [x, p]), _) => {
             let ctx = {
                 let mut ctx = ctx;
                 ctx.insert(x.label.clone(), Type::Num);
                 ctx
             };
-            check_sort_helper(success, ctx, &kids[1], p);
+            check_type_helper(success, ctx, &rule_kids[1].to_type(), &p);
         }
-        // default case that doesn't interact with environment
+        // The proof step cases are handled here
+        ((label, _), Type::Proof(prop)) => match ((label, expr.kids.as_slice()), prop.pat()) {
+            (("intro_and", [a, b]), ("and", [p, q])) => {
+                // let [a, b] = from_expr_kids_to_array(expr.kids);
+                check_type_helper(success, ctx.clone(), &Type::Proof(p), a);
+                check_type_helper(success, ctx.clone(), &Type::Proof(q), b);
+            }
+            (("intro_top", []), ("top", [])) => {}
+            (("elim_bot", [a]), _) => {
+                check_type_helper(success, ctx, &Type::Proof(&ex!(bot)), a);
+            }
+            _ => {
+                add_error("invalid proof".to_owned());
+            }
+        },
+        // default case that doesn't interact with environment, and uses simple sorts
         _ => {
-            for (kid, expected_kid_sort) in expr.kids.into_iter().zip(kids.iter()) {
-                check_sort_helper(success, ctx.clone(), expected_kid_sort, kid);
+            for (kid, expected_kid_sort) in expr.kids.iter().zip(rule_kids.iter()) {
+                check_type_helper(success, ctx.clone(), &expected_kid_sort.to_type(), &kid);
             }
         }
     }
 }
 
-#[derive(Debug, Clone)]
-enum Type {
-    Num,
-    Proof(Expr),
+fn test<'a>(ctx: &HashMap<String, Type<'a>>, expected_prop: AnnExpr<'a>, proof: AnnExpr<'a>) {
+    let prop_well_typed = check_type(ctx.clone(), &Type::Prop, &expected_prop);
+    let expected_prop = expected_prop.forget_ann();
+    let term_well_typed = check_type(ctx.clone(), &Type::Proof(&expected_prop), &proof);
+    println!("");
+    println!("{ctx:?} |-");
+    println!("     {proof}");
+    println!("        : {expected_prop}");
+    println!(" ==> {}", prop_well_typed && term_well_typed);
 }
 
-/// Checks that a proof is valid with respect to an expected [`Prop`].
-/// - Assumes that all the [`Type`]s in the context have been sort-checked.
-/// - Assumes that [`expected_prop`] has been sort-checked.
-fn check_proof(ctx: HashMap<String, Type>, expected_prop: &Expr, proof: Expr) -> Expr {
-    // get the corresponding rule
-    let Rule { sort, kids } = match GRAMMAR.get(&proof.label) {
-        None => return proof.annotate("unknown expr label".to_owned()),
-        Some(rule) => rule,
-    };
-    // check sort
-    if sort != &Proof {
-        return proof.annotate(format!(
-            "expected expr to have sort Proof, but actually has sort {sort}"
-        ));
-    }
-
-    // check arity
-    if proof.kids.len() != kids.len() {
-        let proof_kids_len = proof.kids.len();
-        return proof.annotate(format!(
-            "expected expr to have {} kids, but actually has {} kids",
-            kids.len(),
-            proof_kids_len
-        ));
-    }
-
-    // check validity
-    match (proof.label.as_str(), expected_prop.pat()) {
-        ("intro_and", ("and", [p, q])) => {
-            let [a, b] = from_expr_kids_to_array(proof.kids)
-                .expect("impossible since we already checked the arity of proof");
-            let a = check_proof(ctx.clone(), p, a);
-            let b = check_proof(ctx.clone(), q, b);
-            Expr {
-                label: proof.label,
-                annotation: None,
-                kids: vec![a, b],
-            }
-        }
-        ("intro_top", ("top", [])) => Expr {
-            label: proof.label,
-            annotation: None,
-            kids: vec![],
-        },
-        ("elim_bot", _) => {
-            let [a] = from_expr_kids_to_array(proof.kids)
-                .expect("impossible since we already checked the arity of proof");
-            let a = check_proof(ctx, &ex!["bot"], a);
-            Expr {
-                label: proof.label,
-                annotation: None,
-                kids: vec![a],
-            }
-        }
-        _ => Expr {
-            label: proof.label,
-            annotation: Some(format!(
-                "this proof is not a valid proof of the prop {expected_prop}"
-            )),
-            kids: proof.kids,
-        },
-    }
+pub fn main() {
+    test(
+        &hash_map! {},
+        ex_ann!(and, [ex_ann!(top), ex_ann!(top)]),
+        ex_ann!(intro_and, [ex_ann!(intro_top), ex_ann!(intro_top)]),
+    );
+    test(
+        &hash_map! {},
+        ex_ann!(and, [ex_ann!(top), ex_ann!(bot)]),
+        ex_ann!(intro_and, [ex_ann!(intro_top), ex_ann!(intro_top)]),
+    );
+    test(
+        &hash_map! {},
+        ex_ann!(and, [ex_ann!(bot), ex_ann!(top)]),
+        ex_ann!(intro_and, [ex_ann!(intro_top), ex_ann!(intro_top)]),
+    );
+    test(
+        &hash_map! {},
+        ex_ann!(and, [ex_ann!(bot), ex_ann!(bot)]),
+        ex_ann!(intro_and, [ex_ann!(intro_top), ex_ann!(intro_top)]),
+    );
+    test(
+        &hash_map! {},
+        ex_ann!("and", [ex_ann!(top), ex_ann!(top)]),
+        ex_ann!(
+            intro_and,
+            [
+                ex_ann!(intro_and, [ex_ann!(intro_top), ex_ann!(intro_top)]),
+                ex_ann!(intro_top)
+            ]
+        ),
+    );
 }
-
-// fn test(ctx: &HashMap<String, Type>, expected_prop: Expr, proof: Expr) {
-//     let expected_prop = check_sort_helper(ctx.clone(), &Prop, expected_prop);
-//     let proof = check_sort_helper(ctx.clone(), &Proof, proof);
-//     let result = check_proof(ctx.clone(), &expected_prop, proof.clone());
-//     println!("");
-//     println!("{ctx:?} |-");
-//     println!("     {proof}");
-//     println!("        : {expected_prop}");
-//     println!(" ==> {result}");
-// }
-
-// pub fn main() {
-//     test(
-//         &hash_map! {},
-//         ex!(and, [ex!(top), ex!(top)]),
-//         ex!(intro_and, [ex!(intro_top), ex!(intro_top)]),
-//     );
-//     test(
-//         &hash_map! {},
-//         ex!(and, [ex!(top), ex!(bot)]),
-//         ex!(intro_and, [ex!(intro_top), ex!(intro_top)]),
-//     );
-//     test(
-//         &hash_map! {},
-//         ex!(and, [ex!(bot), ex!(top)]),
-//         ex!(intro_and, [ex!(intro_top), ex!(intro_top)]),
-//     );
-//     test(
-//         &hash_map! {},
-//         ex!(and, [ex!(bot), ex!(bot)]),
-//         ex!(intro_and, [ex!(intro_top), ex!(intro_top)]),
-//     );
-//     test(
-//         &hash_map! {},
-//         ex!("and", [ex!(top), ex!(top)]),
-//         ex!(
-//             intro_and,
-//             [
-//                 ex!(intro_and, [ex!(intro_top), ex!(intro_top)]),
-//                 ex!(intro_top)
-//             ]
-//         ),
-//     );
-// }
